f303ded6794c7fda63fe45840c384b986fe9052e
Lucas Lessa Gomes <lucaslessagomes@gmail.com>
2024-07-30 15:50:33 -0300

    adicionado links das redes no rodape

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -5,7 +5,38 @@ const Rodape = () => {
     <section className='rodape-section'>
       <div className='rodape-circle circle-position'></div>
       <div className='rodape-circle circle-position-2'></div>
-      <div className='rodape-area'></div>
+      <div className='rodape-area'>
+        <div className='rodape-content'>
+          <div>
+            <h3>Contato</h3>
+          </div>
+          <div className='contato-content'>
+            <div className='contato-links'>
+              <a className='div-contato-conteudo' href='http://www.github.com/LLucasLessa' target='_blank'>
+                <div className='div-imagem-contato'>
+                  <img src='./public/img/icons/github-90.png' />
+                </div>
+                <p className='p-contato'>github.com/LLucasLessa</p>
+              </a>
+              <a className='div-contato-conteudo' href='http://www.linkedin.com/in/lucaslessagomes/' target='_blank'>
+                <div className='div-imagem-contato'>
+                  <img src='./public/img/icons/linkedin-90.png' />
+                </div>
+                <p className='p-contato'>linkedin.com/in/lucaslessagomes/</p>
+              </a>
+              <div className='div-contato-conteudo'>
+                <div className='div-imagem-contato'>
+                  <img src='./public/img/icons/email-90.png' />
+                </div>
+                <p className='p-contato'>lucaslessagomes@gmail.com</p>
+              </div>
+            </div>
+            <div className='contato-mensagem'>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
