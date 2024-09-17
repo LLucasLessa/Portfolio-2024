56185a044b410f77faa432c0db5ea70f57ad8eaf
Lucas Lessa Gomes <lucaslessagomes@gmail.com>
2024-09-17 19:24:15 -0300

    c

--- src/componentes/Projetos/index.jsx
+++ src/componentes/Projetos/index.jsx
@@ -1,11 +1,13 @@
 import ItensProjetos from '../ItensProjetos'
 import './projetos.css'
+import imagem from '/src/assets/organoimagecomfundo.png';
+
 
 const Projetos = () => {
   return(
     <section className='projetos-section'>
       <h2 className='projetos-titulo'>Projetos</h2>
-      <ItensProjetos imagem='./src/assets/organoimagecomfundo.png' direcao='!' titulo='Organo' descricao='Projeto desenvolvido em React onde o usuário insere os dados e cadastra um colaborador no sistema, sendo esse sistema separado por times com as suas respectivas cores e nomes.' typescript='none'/>
+      <ItensProjetos imagem={imagem} direcao='!' titulo='Organo' descricao='Projeto desenvolvido em React onde o usuário insere os dados e cadastra um colaborador no sistema, sendo esse sistema separado por times com as suas respectivas cores e nomes.' typescript='none'/>
       
     </section>
   )
